12e10bdfabb0f9e6604a4edf5d0e62d1f1ffa69a
icerahi <42351274+icerahi@users.noreply.github.com>
2018-10-19 00:23:50 +0600

    Add files via upload

--- file.py
+++ file.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,url_for,flash
+from flask import Flask,render_template,url_for,flash,redirect
 from form import RegistrationForm,LoginForm
 
 app=Flask(__name__)
@@ -26,11 +26,20 @@ def profile():
 @app.route("/login",methods=["GET","POST"])
 def login():
     loginform=LoginForm()
+    if loginform.validate_on_submit():
+        if loginform.email.data=="rahi@gmail.com" and loginform.password.data=="rahi":
+            flash("login hass success","success")
+            return redirect(url_for('home'))
+        else:
+            flash("Please check your email and password.And Try Again!","danger")
     return render_template("login.html",loginform=loginform)
 
 @app.route("/register",methods=["GET","POST"])
 def register():
     registrationform=RegistrationForm()
+    if registrationform.validate_on_submit():
+        flash("Account successfully create for %s"%(registrationform.username.data),"success")
+        return redirect(url_for('home'))
     return render_template("register.html",registrationform=registrationform)
 
 
